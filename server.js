da2f180c42f578bf8ec7914b954d1c93be1691bb
Ilya Shchutskiy <i1ya1l@yandex.ru>
2022-08-25 19:09:09 +0300

    feat(*): init

---
+++ server.js
@@ -0,0 +1,28 @@
+const ronin = require('ronin-server');
+const mocks = require('ronin-mocks');
+const database = require('ronin-database');
+
+const server = ronin.server();
+
+database.connect(process.env.CONNECTIONSTRING);
+
+server.use('/foo', (req, res) => res.json({ foo: 'bar' }));
+
+server.use('/', mocks.server(server.Router(), false, true));
+server.start();
+
+/*
+docker run -it --rm -d -v node-docker-mongodb:/data/db \
+  -v node-docker-mongodb_config:/data/configdb -p 27017:27017 \
+  --network node-docker-mongodb \
+  --name mongodb \
+  mongo
+
+docker run \
+  -it --rm -d \
+  --network node-docker-mongodb \
+  --name rest-server \
+  -p 8000:8000 \
+  -e CONNECTIONSTRING=mongodb://mongodb:27017/notes \
+  node-docker
+*/
